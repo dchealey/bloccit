90bec94f0e73ceb3ed76ec79400f26829c1fcc8c
David <davidchristopherhealey@gmail.com>
2016-09-07 12:42:07 +0000

    Checkpoint 38: snag

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
   validates :password, length: { minimum: 6 }, allow_blank: true
-  validates :email,
+  validates :email, 
     presence: true,
-    uniqueness: {case_sensitive: false },
+    uniqueness: { case_sensitive: false },
     length: { minimum: 3, maximum: 254 }
   has_secure_password
 end
